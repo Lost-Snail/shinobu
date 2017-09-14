18facc0c7ee737ab9f4cfe1c1071e3bce6401bb6
Théo <rbshayla@yahoo.co.jp>
2017-09-14 16:07:52 +0200

    cleaned repo

--- Swift/Package.swift
+++
@@ -1,8 +0,0 @@
-import PackageDescription
-
-let package = Package(
-    name: "Swift",
-    dependencies: [
-        .Package(url: "https://github.com/Azoy/Sword", majorVersion: 0, minor: 8)
-    ]
-)
